b1a5804870ab94371cb2a80623350b1ed4b70aca
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2022-04-22 15:59:16 -0500

    Initial Commit

---
+++ EldenMessages/EldenMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EldenMessagesApp.swift
+//  EldenMessages
+//
+//  Created by Dustin Olsen on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct EldenMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
